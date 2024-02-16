ac4746716e5616091abdfac562ae5682c1b51a61
Shifyannn <asifyan01@gmail.com>
2024-02-17 00:14:28 +0700

    Penambahan Halaman Detail

--- index.js
+++ index.js
@@ -10,9 +10,18 @@ app.set('view engine', 'ejs');
 // Static file view
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
 	let database = loadDatabase();
 	res.render('home', { database });
+});
+
+app.get('/home/:id', (req, res) => {
+	let database = loadDatabase();
+	res.render('home', { database });
+});
+
+app.get('/detail', (req, res) => {
+	res.render('dataDetail');
 });
 
 app.listen(port, () => {
