80662d38c0f1c0b3b8a7d1da8b38ba043f7ebab9
Shifyannn <asifyan01@gmail.com>
2024-02-18 14:03:48 +0700

    PEnambahan rute tambah data

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 // Set EJS as View Engine
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/home', (req, res) => {
 	let database = loadDatabase();
@@ -16,13 +17,20 @@ app.get('/home', (req, res) => {
 app.get('/addData', (req, res) => {
 	res.render('addData');
 });
-
+app.post('/home', (req, res) => {
+	console.log(req.body);
+	res.send('Data Dikirim');
+});
 app.get('/home/:nama', (req, res) => {
 	let myData = findData(req.params.nama);
 	// console.log(req.params.nama);
 	res.render('dataDetail', { myData });
 });
 
+app.use((req, res) => {
+	res.status(404);
+	res.send('<h1>Halaman Tidak Ditemukan</h1>');
+});
 // Static file view
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
